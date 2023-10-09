352ec4ded83e0a4063f5d698c18abcecb0c0991e
salinpaul valooran <salinpv@hotmail.com>
2023-10-10 00:40:30 +0530

    let const

---
+++ examples/chapter01_02/10-letconst.js
@@ -0,0 +1,23 @@
+let framework = "Angular";
+console.log(framework);
+// let framework = "React";
+// console.log(framework);
+
+let language = "JavaScript";
+console.log(language);
+
+const PI = 3.14;
+console.log(PI);
+
+// PI = 3.0;   throws errors
+
+const jsFramework = {
+  name: "Angular",
+};
+jsFramework.name = "React";
+
+// error, cannot reassign object reference
+//
+// isFramework = {
+// name: "Vue"
+// };
