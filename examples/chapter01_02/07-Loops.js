c0e883f3740bebbb447e7af2c64fce98ff6353c4
salinpaul valooran <salinpv@hotmail.com>
2023-10-09 23:46:55 +0530

    loops

---
+++ examples/chapter01_02/07-Loops.js
@@ -0,0 +1,19 @@
+console.log("**** for example ****");
+// for - example
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log("**** while example ****");
+let j = 0;
+while (j < 10) {
+  console.log(j);
+  j++;
+}
+
+console.log("**** do-while example ****");
+let k = 0;
+do {
+  console.log(k);
+  k++;
+} while (k < 10);
